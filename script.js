71a1f58d39232b0318bae213baa2dd5dac6b6634
kpatterson01 <kpatterson01@users.noreply.github.com>
2018-11-05 11:21:48 -0500

    added website

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function button1Function() { //button 1 alert box
-  window.alert(" am an aleert box");
+  window.alert("The start / stop stations that are most popular are Start Station at 3030 calculating at 9319 and the most popular stop station is 3069 calculating at 10254.");
 }
 
 function button2Function() { //button 2 alert box
-  window.alert(" am an aleert box");
+  window.alert("The average distance travel is 400.345mph");
 }
 
 function button3Function() { //button 3 alert box
-  window.alert(" am an aleert box");
+  window.alert("About 500 riders include bike sharing as a regular part of their commute.");
 }
 
-function button4Function() { //button 3 alert box
+function button4Function() { //button 3 alert bosx
   window.alert("Your bike made it to the finish line!!!");
 }
